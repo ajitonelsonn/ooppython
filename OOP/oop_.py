f05147df3aa3f41ea858317957ec39ded63cf35b
Ajito Nelson <8997ajito@gmail.com>
2024-04-16 16:27:43 +0900

    Update oop_.py

--- OOP/oop_.py
+++ OOP/oop_.py
@@ -3,10 +3,14 @@
 #Structural > Procedural - Programing - Di esekusi berdasarkan urutan --Serial
 #Object > Oriented - Programing - Semua di angaap object/instanse - karik buat n'ebe hanesan ita halo template or class
 
+# Object-Oriented Programming
+
 #Ezemplu class hero: 
 #Tamba nia nu'dar template entaun nia iha valor rua mak hanesan:
 #1. atrribute hanesan "Ne'ebe hero iha": naran, power, deffence
 #2. Method "mak ida ne'ebe hero bele halo" : meneyerang, bertahan, begerak
 
-# Object-Oriented Programming
+
+#Pois berdasarkan template iha leten ne'e agora ita halo object
+#object sei foti iha leten
 
